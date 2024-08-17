8ad9a159304d53b3ca137fb1759c6b9c5dff04f9
unknown <samwelproductions@gmail.com>
2024-08-17 01:43:22 -0500

    late nite

--- README.md
+++ README.md
@@ -8,10 +8,21 @@ This repository assumes that you have the LTS (20.16.0) version of node.js insta
 
 For initial installation - please refer to the [Official Node.js download page](https://nodejs.org/en/download/)
 
+After intallation you will be able to execute the code, with the test suite by running 
+
+`python3 evaluateShared.py --cmd "node index.js" --problemDir "./problems"`
+
+To run the codebase without the test suite, you can run 
+
+`node index.js {problem_path}` where problem_path is the path to a problem file, as describe within the challenge document.
+
 ## Tools Used
 
 For the building of this project here are all of the resources I used
 1) a lot of the API search was done on the [Node.js reference page](https://nodejs.org/docs/latest-v20.x/api/index.html).
 2) Regular Expressions were written using google, and https://regex101.com/
-3) A [random article about Math](https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-243.php) - so I was 100% sure Math.hypot was the same as Euclidean distance.
+3) A [random article about Math](https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-243.php) - so I was 100% sure Math.hypot was the same as Euclidean distance. I ended up not even using this source, but it was an interesting read. 
 
+## Final Comments 
+
+This codebase makes a numerious bunch of assumptions. We assume that the file input will always be present, and that many amounts of errors (such as typing issues, or runtime problems) should not happen under the assumed scenarios outlined in the challenge. 
--- index.js
+++ index.js
@@ -37,52 +37,106 @@ class Point {
     this.x = x;
     this.y = y;
   }
-  distanceToPoint(point) {
-    // Usually I would just do the full math equations
-    // Math.sqrt(Math.pow((returnObject.end.x - returnObject.start.x), 2) + Math.pow((returnObject.end.y - returnObject.start.y), 2));
-    // But since we are trying to make this fast - we use Math.hypot instead.
-
-    // The calculate goes 2nd x - 1st x, 2nd y - 1st y
-    return Math.hypot((point.x - this.x), (point.y - this.y));
+  static distanceBetweenPoints(point1, point2) {
+  // Turns out - Math.hypot is slower then just doing the calculation ourselves.
+  // return Math.hypot((point2.x - point1.x), (point2.y - point1.y));
+  return Math.sqrt(Math.pow((point2.x - point1.x), 2) + Math.pow((point2.y - point1.y), 2));
   }
 }
 
+/**
+ * Route class - which is pretty much just an array of loads.
+ */
 class Route {
   static depotOrigin = Point.origin;
   constructor(loads) {
     // A route is basically just the stops we need to do (loads), and the points to go.
     // An array of Load objects
     this.loads = loads;
-    // Get the distance between the depot origin and the first load starting point.
-    this.homeDistance = depotOrigin.distanceToPoint(this.firstLoad.start);
-    // Get the distance between the last load and the depot origin. That way our driver can go home.
-    this.finalPoint.origin.distanceToPoint(this.lastLoad.end);
   }
 
-  calculateRouteDistance() {
-    // Get the distance between the depot origin and the first load starting point.
-    this.homeDistance = this.depotOrigin.distanceToPoint(this.firstLoad.start);
-    // Get the distance between the last load and the depot origin. That way our driver can go home.
-    this.finalPoint.origin.distanceToPoint(this.lastLoad.end);
-  }
-  get firstLoad() {
-    return this.loads.length !== 0 ? this.loads[0] : undefined;
-  }
-  get lastLoad() {
-    return this.loads.length !== 0 ? this.loads[this.loads.length - 1] : undefined;
+  /**
+   * Getter for calculating the total distance of the route.
+   * @returns The total distance of the route, with home stops included.
+   */
+  get totalDistance() {
+    let currentPosition = Point.origin;
+    let totalDistance = 0;
+    
+    for (const currentLoad of this.loads) {
+        totalDistance += Point.distanceBetweenPoints(currentPosition, currentLoad.start)
+        currentPosition = currentLoad.start
+        totalDistance += Point.distanceBetweenPoints(currentPosition, currentLoad.end)
+        currentPosition = currentLoad.end
+      }
+      totalDistance += Point.distanceBetweenPoints(currentPosition, Point.origin)
+      return totalDistance;
   }
 }
+/**
+ * Load class - represents a load to be delivered
+ */
 class Load { 
   constructor(loadNumber, startPoint, endPoint) {
     this.loadNumber = loadNumber;
     // These are assumed to be point Objects from the class
     this.start = startPoint;
     this.end = endPoint;
-    // We just call the other classes function.
-    this.distance = this.start.distanceToPoint(this.end);
+    this.isAssigned = false;
   }
 }
 
+/**
+ * Central function to calculate the potential distance of a new load, if added to a route
+ * @param {Array<Load>} loadArray existing load array
+ * @param {Load} newLoad load to add
+ * @returns returns the total distance of the route if the new load is added.
+ */
+function calculatePotentialDistance(loadArray, newLoad) {
+  if (loadArray.length === 0) {
+    const fakeRoute = new Route([newLoad]);
+    return fakeRoute.totalDistance;
+  } else {
+    const fakeRoute = new Route([...loadArray, newLoad]);
+    return fakeRoute.totalDistance;
+  }
+}
+
+/**
+ * Creates and assigns drivers based on loads 
+ * Will add a new driver if existing drivers cannot take on a load
+ * @param {Array<Load>} loads - An array of Load objects
+ * @returns An array of drivers, each with an array of loads.
+ */
+function assignDrivers(loads) {
+  // Start with no drivers
+  const drivers = [{ loads:[] }];
+  // iterate over each load
+  for (const load of loads) {
+    for (const driver of drivers) {
+      const potentialNewDistance = calculatePotentialDistance(driver.loads, load);
+      if (potentialNewDistance <= 720 && !load.isAssigned) { 
+        load.isAssigned = true;
+        driver.loads.push(load);
+        // We want to leave the loop if we assign a load.
+        break;
+      } 
+    }
+    // If we haven't assigned the load and went thru all of our drivers, we need to create a new driver.
+    if (!load.isAssigned) {
+      load.isAssigned = true;
+      const newDriver = { loads: [load] };
+      drivers.push(newDriver);
+    }
+  }
+  return drivers;
+}
+
+/**
+ * Parses a line from a VRP problem file
+ * @param {string} lineString a string - assuming to be points and load information
+ * @returns A new load object
+ */
 function parseLine(lineString) {
   // Lots of stuff - this parses the line into an object.
   const parseMyLineRegex = /(\d+)\s+\(([^,]+),([^)]+)\)\s+\(([^,]+),([^)]+)\)/;
@@ -93,7 +147,15 @@ function parseLine(lineString) {
   return loadParsed;
 }
 
+// Parse them args
+const args = process.argv.slice(2);
+const filePath = args[0];
 // We are using top-level await here. 
 // That might not work depending on the version of Node.js the user uses.
-const loads = await parseProblemFile("./problems/1.txt");
-console.error("My loads", loads);
+const loads = await parseProblemFile(filePath);
+// Well now we have our list, lets get it printed.
+const drivers = assignDrivers(loads);
+for (const driver of drivers) {
+  const loadIds = driver.loads.map(load => load.loadNumber).join(",");
+  console.log(`[${loadIds}]`);
+}
